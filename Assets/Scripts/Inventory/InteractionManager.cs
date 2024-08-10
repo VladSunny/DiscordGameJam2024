376428ea3151a0f704f39c3746c37a36cb8e4e8a
Vladislav Sungurov <vladislavsunny@gmail.com>
2024-08-10 18:01:23 +0300

    Interaction Popup

---
+++ Assets/Scripts/Inventory/InteractionManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using DG.Tweening;
+
+namespace Scripts.PlayerInventory
+{
+    public class InteractionManager : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private GameObject _interactionPopup;
+
+        [Header("UI Settings")]
+        [SerializeField] private float _popupOffset = 1.5f;
+
+        private Interactable _currentInteractable;
+
+        private void Awake()
+        {
+            _currentInteractable = null;
+        }
+
+        public void SeeItem(Interactable interactable)
+        {
+            Debug.Log("See item: " + interactable);
+
+            if (interactable == null)
+            {
+                _interactionPopup.SetActive(false);
+                _currentInteractable = null;
+                return;
+            }
+
+            if (_currentInteractable != interactable)
+            {
+                _currentInteractable = interactable;
+                _interactionPopup.SetActive(true);
+                _interactionPopup.transform.position = interactable.transform.position;
+                _interactionPopup.transform.DOMoveY(interactable.transform.position.y + _popupOffset, 0.5f).SetEase(Ease.OutQuad);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Inventory/ItemInteraction.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+namespace Scripts.PlayerInventory
+{
+    public class ItemInteraction : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private InteractionManager _interactionManager;
+
+        [Header("Settings")]
+        [SerializeField] private float sphereRadius = 5f;
+        [SerializeField] private LayerMask interactableLayer;
+
+        private void Update()
+        {
+            Vector3 position = transform.position;
+
+            Collider[] hitColliders = Physics.OverlapSphere(position, sphereRadius, interactableLayer);
+
+            if (hitColliders.Length > 0)
+            {
+                Collider closestCollider = null;
+                float closestDistance = Mathf.Infinity;
+
+                foreach (var collider in hitColliders)
+                {
+                    float distance = Vector3.Distance(position, collider.transform.position);
+
+                    if (distance < closestDistance)
+                    {
+                        closestDistance = distance;
+                        closestCollider = collider;
+                    }
+                }
+
+                _interactionManager.SeeItem(closestCollider.GetComponent<Interactable>());
+            }
+
+            else { _interactionManager.SeeItem(null); }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, sphereRadius);
+        }
+    }
+}
